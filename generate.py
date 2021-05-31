55b3c0fd1a7d59054f11a2036d38d90ffd9fed30
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-05-31 16:10:19 +0200

    Write static Front Matter to markdown document

--- generate.py
+++ generate.py
@@ -1,3 +1,11 @@
-f = open("optimization.md", "a")
-f.write("Hello, World!")
+f = open("optimization.md", "w")
+f.write('''---
+title: "Optimization benchmarks"
+date: 2021-05-26T08:55:49+02:00
+weight: 1
+description: >
+  Benchmarks of tests ran in optimization scheme.
+
+  Results are compared with [`13a4c1d`](https://github.com/chocoteam/choco-solver/commit/13a4c1dca0dd58d62acc741866fb945f3fe81592).
+---''')
 f.close()
