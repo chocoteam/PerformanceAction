cbfa741f2d22e24abb5710efd92f2a53837ec5ba
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-05-31 16:26:53 +0200

    Add string interpolation for commit hash and date time

--- generate.py
+++ generate.py
@@ -1,11 +1,16 @@
-f = open("optimization.md", "w")
-f.write('''---
+import datetime
+
+# Variables
+commit = '13a4c1dca0dd58d62acc741866fb945f3fe81592'
+
+file = open("optimization.md", "w")
+file.write(f'''---
 title: "Optimization benchmarks"
-date: 2021-05-26T08:55:49+02:00
+date: {datetime.datetime.now().astimezone().isoformat()}
 weight: 1
 description: >
   Benchmarks of tests ran in optimization scheme.
 
-  Results are compared with [`13a4c1d`](https://github.com/chocoteam/choco-solver/commit/13a4c1dca0dd58d62acc741866fb945f3fe81592).
+  Results are compared with [`{commit[0:7]}`](https://github.com/chocoteam/choco-solver/commit/{commit}).
 ---''')
-f.close()
+file.close()
