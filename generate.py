9fd1852eac0cf1b6046730cf3e1fc35ba0687b82
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-05-31 16:27:36 +0200

    Add comments

--- generate.py
+++ generate.py
@@ -3,7 +3,10 @@ import datetime
 # Variables
 commit = '13a4c1dca0dd58d62acc741866fb945f3fe81592'
 
+# Open file
 file = open("optimization.md", "w")
+
+# Write Front Matter
 file.write(f'''---
 title: "Optimization benchmarks"
 date: {datetime.datetime.now().astimezone().isoformat()}
@@ -13,4 +16,6 @@ description: >
 
   Results are compared with [`{commit[0:7]}`](https://github.com/chocoteam/choco-solver/commit/{commit}).
 ---''')
+
+# Close file
 file.close()
