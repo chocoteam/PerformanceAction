2ac6f212317a6367f784b2d3606bc8c74bedddb2
Rémi Bardon <remi.bardon.dev@gmail.com>
2021-06-03 12:31:54 +0200

    Lint

--- src/git.py
+++ src/git.py
@@ -1,21 +1,24 @@
 import subprocess
 import os
 
-def get_filecontent(filepath,filehash):
-    filepath = filepath.replace("\\","/") # to get rid of \ on Windows
+def get_filecontent(filepath: str, filehash: str):
+    filepath = filepath.replace('\\', '/') # Get rid of `\` on Windows
 
     # Get absolute path to current Git repo
     try:
-        repopath = str(subprocess.check_output("git rev-parse --show-toplevel")).lstrip("b'").rstrip("n'").rstrip("\\")
+        repopath = str(subprocess.check_output('git rev-parse --show-toplevel'))
+            .lstrip("b'").rstrip("n'").rstrip('\\') # Clean output
     except subprocess.CalledProcessError as e:
-        print("Could not determine the Git repo path. Error : " + str(e.returncode))
+        print(f'Could not determine the Git repo path. Error : ${e.returncode}')
 
-    relpath = os.path.relpath(filepath,repopath).replace("\\","/") # to get rid of \ on Windows
+    relpath = os.path.relpath(filepath, repopath)
+        .replace('\\', '/') # Get rid of `\` on Windows
 
     # Get file content from relpath and filehash
     try:
-        filecontent = str(subprocess.check_output("git show " + filehash + ":" + relpath)).lstrip("b'").rstrip("'").replace("\\n","")
+        filecontent = str(subprocess.check_output('git show ${filehash}:${relpath}'))
+            .lstrip("b'").rstrip("'").replace('\\n', '') # Clean output
     except subprocess.CalledProcessError as e:
-        print("Could not read " + filepath + " from " + filehash + ". Error : " + str(e.returncode))
+        print(f'Could not read ${filepath} from ${filehash}. Error : ${e.returncode}')
 
     return filecontent
--- src/main.py
+++ src/main.py
@@ -6,30 +6,30 @@ import filemanager
 def main():
     # Argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument("filepath", help="path to json to parse")
-    parser.add_argument("hashref", help="commit hash to reference json")
-    parser.add_argument("hashcomp", help="commit hash to compare with")
+    parser.add_argument('filepath', help='Path of JSON output file to parse')
+    parser.add_argument('hashref', help='Hash value of commit to compare against (reference commit)')
+    parser.add_argument('hashcomp', help='Hash value of commit to compare')
     args = parser.parse_args()
 
     filepath = os.path.abspath(args.filepath)
     hashref = args.hashref
     hashcomp = args.hashcomp
 
-    print("Your filepath : " + filepath)
-    print("Your reference commit hash : " + hashref)
-    print("Your commit hash to compare with : " + hashcomp)
-    print("-----------------------------------------------")
+    print(f'Your filepath: ${filepath}')
+    print(f'Your reference commit hash: ${hashref}')
+    print(f'Your commit hash to compare: ${hashcomp}')
+    print('-----------------------------------------------')
 
     # File parsing
-    ref_file = filemanager.FileManager(filepath,hashref)
-    comp_file = filemanager.FileManager(filepath,hashcomp)
+    ref_file = filemanager.FileManager(filepath, hashref)
+    comp_file = filemanager.FileManager(filepath, hashcomp)
 
     ref_file.parse()
-    print("Content of " + ref_file.filepath + " " + ref_file.filehash)
+    print(f'Content of ${ref_file.filepath} ${ref_file.filehash}:')
     print(ref_file.content)
 
     comp_file.parse()
-    print("Content of " + comp_file.filepath + " " + comp_file.filehash)
+    print(f'Content of ${comp_file.filepath} ${comp_file.filehash}:')
     print(comp_file.content)
 
 if __name__ == "__main__":
