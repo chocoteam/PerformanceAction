aa78debe339097e0c2faed54377df75145df8f95
FlorianDussable <floriandussable@gmail.com>
2021-06-14 18:08:42 +0200

    add feature to handle multiple files from json source dir

--- src/action.py
+++ src/action.py
@@ -2,12 +2,13 @@ import argparse
 import os
 
 from main import shared_main
+from git import get_changed_jsons
 from filemanager import FileManager
 
 def main():
     # Argument parsing
     parser = argparse.ArgumentParser()
-    parser.add_argument('filepath', help='Path of JSON output file to parse')
+    parser.add_argument('jsondir', help='Path of JSON directory to parse files from')
     parser.add_argument('hashref', help='Hash value of commit to compare against (reference commit)')
     parser.add_argument('hashcomp', help='Hash value of commit to compare')
     parser.add_argument('output_path', help='Path of folder to output the generated page')
@@ -15,24 +16,26 @@ def main():
     parser.add_argument('similar_percent_limit', help='Maximum percentage signifying similarity. It must be positive, as it will be checked for both lower and higher values. If not set, it will default to `1%`', type=float, default=1)
     args = parser.parse_args()
 
-    filepath = os.path.abspath(args.filepath)
+    jsondir = os.path.abspath(args.jsondir)
     hashref = args.hashref
     hashcomp = args.hashcomp
     output_path = os.path.abspath(args.output_path)
 
-    print(f'Your filepath: {filepath}')
+    print(f'Your JSON directory: {jsondir}')
     print(f'Your reference commit hash: {hashref}')
     print(f'Your commit hash to compare: {hashcomp}')
     print('-----------------------------------------------')
 
-    # File parsing
-    ref_file = FileManager(filepath, hashref)
-    comp_file = FileManager(filepath, hashcomp)
-    ref_content = ref_file.parse()
-    comp_content = comp_file.parse()
+    changed_jsons = get_changed_jsons(jsondir, hashcomp)
+    for changed_json in changed_jsons:
+        # File parsing
+        ref_file = FileManager(changed_json, hashref)
+        comp_file = FileManager(changed_json, hashcomp)
+        ref_content = ref_file.parse()
+        comp_content = comp_file.parse()
 
-    # Rest of the program
-    shared_main(filepath, ref_content, comp_content, output_path, args.repository_url, args.similar_percent_limit)
+        # Rest of the program
+        shared_main(changed_json, ref_content, comp_content, output_path, args.repository_url, args.similar_percent_limit)
 
 if __name__ == "__main__":
     main()
--- src/git.py
+++ src/git.py
@@ -1,18 +1,20 @@
 import subprocess
 import os
 
-def get_filecontent(filepath: str, filehash: str):
-    # Get rid of `\` on Windows
-    filepath = filepath.replace('\\', '/')
-
+def get_repo_path():
     # Get absolute path to current Git repo
     try:
         repopath = str(subprocess.check_output(['git', 'rev-parse', '--show-toplevel']))
         # Clean output
         repopath = repopath.lstrip("b'").rstrip("n'").rstrip('\\')
+        return repopath
     except subprocess.CalledProcessError as e:
         print(f'Could not determine the Git repo path. Error: {e.returncode}')
 
+def get_filecontent(filepath: str, filehash: str):
+    # Get rid of `\` on Windows
+    filepath = filepath.replace('\\', '/')
+    repopath = get_repo_path()
     relpath = os.path.relpath(filepath, repopath)
     # Get rid of `\` on Windows
     relpath = relpath.replace('\\', '/')
@@ -26,3 +28,17 @@ def get_filecontent(filepath: str, filehash: str):
         print(f'Could not read {filepath} from {filehash}. Error: {e.returncode}')
 
     return filecontent
+
+def get_changed_jsons(path: str, commit_hash: str):
+    repopath = get_repo_path()
+    # Get list of changed files in specific path for a specific commit
+    try:
+        changed_jsons = str(subprocess.check_output(['git', 'diff-tree', '--no-commit-id', '--name-only', '-r', commit_hash, path]))
+        # Clean output
+        changed_jsons = changed_jsons.lstrip("b'").rstrip("n'").rstrip('\\')
+        changed_jsons = changed_jsons.split('\\n')
+        for changed_json in changed_jsons:
+            changed_json = os.path.join(repopath, changed_json)
+        return changed_jsons
+    except subprocess.CalledProcessError as e:
+        print(f'Could not find any modified json in {path} for commit {commit_hash}')
